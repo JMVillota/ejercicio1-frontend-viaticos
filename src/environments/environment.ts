e9c324c165a75f60e6392e2779f476eaa88e10b8
JMVillota <jeffersonvillota@gmail.com>
2025-01-18 18:06:00 -0500

    cambios_0.0.3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     apiUrl: 'https://charming-illumination-production.up.railway.app/api',
     basicAuth: {
-        username: import.meta.env.VITE_AUTH_USERNAME,
-        password: import.meta.env.VITE_AUTH_PASSWORD
+        username: import.meta.env.VITE_AUTH_USERNAME || '',
+        password: import.meta.env.VITE_AUTH_PASSWORD || ''
     }
 };
